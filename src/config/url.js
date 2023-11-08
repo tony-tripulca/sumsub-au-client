73995fc6dec107e50c8568e6e205e1d5cbf53722
tony-tripulca <tony.tripulca@gmail.com>
2023-11-09 02:25:08 +0800

    pointed to live host

--- src/config/url.js
+++ src/config/url.js
@@ -3,7 +3,7 @@ class URL {
     let env = process.env.REACT_APP_ENV;
 
     if (env === "dev") {
-      return "http://localhost:1337";
+      return "http://nadescrib.com:1337";
     } else if (env === "uat") {
       return "/";
     } else if (env === "prod") {
